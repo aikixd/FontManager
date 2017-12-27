6dfc75f2907b3e48fd2da41f19ca37a39286efe9
Michael Gotfrid <michaelg@codevalue.net>
2017-12-27 11:35:36 +0200

    Get fonts from folder

--- BadPanda.FontManager/BadPanda.FontManager.Wpf/Models/FontModel.cs
+++ BadPanda.FontManager/BadPanda.FontManager.Wpf/Models/FontModel.cs
@@ -1,11 +1,35 @@
-﻿namespace BadPanda.FontManager.Wpf.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using BadPanda.FontManager.Core;
+using BadPanda.FontManager.Cqrs;
+
+namespace BadPanda.FontManager.Wpf.Models
 {
     public interface IFontModel
     {
-        
+        IObservable<IEnumerable<FontWeight>> Fonts { get; }
+        Task ReadFromDirectoryAsync(string path);
     }
-    public class FontModel: IFontModel
+    public class FontModel : IFontModel
     {
-        
+        private readonly Commands _commands;
+        private readonly Subject<IEnumerable<FontWeight>> _fontsSubject = new Subject<IEnumerable<FontWeight>>();
+
+        public IObservable<IEnumerable<FontWeight>> Fonts => _fontsSubject.AsObservable();
+
+
+        public FontModel(Commands commands)
+        {
+            _commands = commands;
+        }
+
+        public async Task ReadFromDirectoryAsync(string path)
+        {
+            var fonts = await _commands.ReadFontWeightsAsync(new Uri(path));
+            _fontsSubject.OnNext(fonts);
+        }
     }
 }
--- BadPanda.FontManager/BadPanda.FontManager.Wpf/ViewModels/FontsDisplayViewModel.cs
+++ BadPanda.FontManager/BadPanda.FontManager.Wpf/ViewModels/FontsDisplayViewModel.cs
@@ -1,17 +1,87 @@
-﻿using BadPanda.FontManager.Wpf.Models;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Windows.Forms;
+using System.Windows.Media;
+using BadPanda.FontManager.Core;
+using BadPanda.FontManager.Wpf.Messages;
+using BadPanda.FontManager.Wpf.Models;
 using Caliburn.Micro;
 
 namespace BadPanda.FontManager.Wpf.ViewModels
 {
-    public interface IFontViewModel{}
+    public interface IFontViewModel { }
 
-    public class FontsDisplayViewModel: PropertyChangedBase, IFontViewModel
+    public class FontsDisplayViewModel : PropertyChangedBase, IFontViewModel, IHandle<DisplayValueChanged>
     {
         private readonly IFontModel _fonts;
+        private string currentText = string.Empty;
 
-        public FontsDisplayViewModel(IFontModel fonts)
+        public BindableCollection<FontViewModel> Fonts { get; set; }
+
+        public FontsDisplayViewModel(IFontModel fonts, IEventAggregator eventAggregator)
         {
             _fonts = fonts;
+            Fonts = new BindableCollection<FontViewModel>();
+
+            eventAggregator
+                .Subscribe(this);
+
+            _fonts
+                .Fonts
+                .ObserveOnDispatcher()
+                .Subscribe(f =>
+                {
+                    Fonts.Clear();
+                    Fonts.AddRange(f.Select(i => new FontViewModel(i, currentText)));
+                });
+        }
+
+        public void Handle(DisplayValueChanged message)
+        {
+            currentText = message.Text;
+            foreach (var f in Fonts)
+                f.Message = message.Text;
+        }
+
+        public void Open()
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    _fonts.ReadFromDirectoryAsync(fbd.SelectedPath);
+            }
+        }
+    }
+
+    public class FontViewModel : PropertyChangedBase
+    {
+        private readonly FontWeight _font;
+        private string _message;
+        private FontFamily _fontFamily;
+
+        public FontFamily FontFamily { get; }
+
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                if (value == _message) return;
+                _message = value;
+                NotifyOfPropertyChange(() => Message);
+            }
+        }
+
+        public FontViewModel(FontWeight font, string startMessage)
+        {
+            _font = font;
+            Message = startMessage;
+            var a = $"{_font.Path.AbsoluteUri}#{_font.Name.Value}";
+            FontFamily = new FontFamily(a);
         }
     }
 }
